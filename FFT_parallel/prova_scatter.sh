dbd200523e2c0e1fa93d14a19223a227360da8c9
CawaAlreadyTaken <daniele.cabassi01@gmail.com>
2023-12-24 18:00:20 +0100

    Let's try with scatter

---
+++ FFT_parallel/prova_scatter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#PBS -l select=1:ncpus=8:mem=16gb
+
+#PBS -l walltime=0:07:00
+
+#PBS -q short_cpuQ
+
+module load mpich-3.2
+mpirun.actual -n 8 HPC_Parallel-FFT/FFT_parallel/prova_scatter HPC_Parallel-FFT/FFT_parallel/
