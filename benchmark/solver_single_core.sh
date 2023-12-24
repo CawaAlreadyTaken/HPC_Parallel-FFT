a9beac525214b463e9ceb7d878312bc05bab4043
CawaAlreadyTaken <daniele.cabassi01@gmail.com>
2023-12-24 14:38:46 +0100

    Renamed and created solver with a single core

---
+++ benchmark/solver_single_core.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#PBS -l select=1:ncpus=1:mem=2gb
+
+#PBS -l walltime=0:04:00
+
+#PBS -q short_cpuQ
+
+module load mpich-3.2
+mpirun.actual -n 32 HPC_Parallel-FFT/FFT_parallel/parallel_solver_0 HPC_Parallel-FFT/FFT_parallel/
