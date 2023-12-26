d188fc66b33050184c5e72832f4477540025668a
CawaAlreadyTaken <daniele.cabassi01@gmail.com>
2023-12-26 13:23:18 +0100

    New dataset size

---
+++ dataset/creation/create_dataset_0_5.py
@@ -0,0 +1,18 @@
+from random import randint
+
+LOG2_DEGREE = 29
+MAX_COEFFICIENT_MODULO = 100
+
+OUTPUT_FILE = "../data/dataset_0_5.txt"
+
+degree = pow(2, LOG2_DEGREE)
+print(f"Creating input type 0 of degree {degree}...")
+
+with open(OUTPUT_FILE, 'w') as f:
+    f.write(str(degree) + "\n")
+    for _ in range(degree):
+        coefficient = randint(-MAX_COEFFICIENT_MODULO, MAX_COEFFICIENT_MODULO)
+        f.write(str(coefficient) + " ")
+    f.write("\n")
+
+print(f"[*] Done. Dataset saved in {OUTPUT_FILE}")
