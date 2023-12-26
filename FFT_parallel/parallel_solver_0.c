384447c30ddbc39e85d2844f05dd88adbecc1664
CawaAlreadyTaken <daniele.cabassi01@gmail.com>
2023-12-26 03:38:48 +0100

    Updated parallel solver 0 for getting timings

--- FFT_parallel/parallel_solver_0.c
+++ FFT_parallel/parallel_solver_0.c
@@ -246,7 +246,7 @@ int main(int argc, char* argv[]) {
 		strcat(full_timings_file, timings_file_name);
 		FILE *timings_file = fopen(full_timings_file, "w");
 		// Opening file for reading input
-		const char *input_file_name = "../dataset/data/dataset_0_3.txt";
+		const char *input_file_name = "../dataset/data/dataset_0_4.txt";
 		int input_file_length = strlen(argv[1]) + strlen(input_file_name) + 1;
 		char *full_input_file = (char *)malloc(input_file_length);
 		strcpy(full_input_file, argv[1]);
--- FFT_parallel/parallel_solver_0.sh
+++ FFT_parallel/parallel_solver_0.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-#PBS -l select=16:ncpus=4:mem=4gb
+#PBS -l select=1:ncpus=1:mem=4gb -l place=pack:excl
 
 #PBS -l walltime=0:10:00
 
 #PBS -q short_cpuQ
 
+#PBS -e easd
+#PBS -o oasd
+
 module load mpich-3.2
-mpirun.actual -n 64 HPC_Parallel-FFT/FFT_parallel/parallel_solver_0 HPC_Parallel-FFT/FFT_parallel/
+mpirun.actual -n 1 HPC_Parallel-FFT/FFT_parallel/parallel_solver_0 HPC_Parallel-FFT/FFT_parallel/
