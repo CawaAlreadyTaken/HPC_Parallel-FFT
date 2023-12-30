1f99df0f973b3682f739403d4b732c288fba7b60
Riccardo Nicolin <riccardo.nicolin@studenti.unitn.it>
2023-12-30 11:04:50 +0100

    debug 8

--- FFT_parallel/pragma.c
+++ FFT_parallel/pragma.c
@@ -17,7 +17,7 @@ int main(){
     int i;
     #pragma omp master
     {
-        #pragma omp for num_threads(5)
+        #pragma omp for
         for (i=0; i<5; i++){
             printf("thread id: %d \n", omp_get_thread_num()); //print thread id to check if oprnmp works
         }
