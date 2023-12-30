063a6e96a1ac70d4c0bef37c06f94df118ad5098
Riccardo Nicolin <riccardo.nicolin@studenti.unitn.it>
2023-12-30 10:52:04 +0100

    debug 6

---
+++ FFT_parallel/pragma.c
@@ -0,0 +1,24 @@
+#ifdef _OPENMP
+    #include <omp.h>
+#else
+    #define omp_get_num_threads() 0
+    #define omp_get_thread_num() 0
+#endif
+
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+#include <time.h>
+#include <mpi.h>
+
+int main(){
+    int i;
+    #pragma omp parallel for num_threads(5)
+    for (i=0; i<5; i++){
+        printf("thread id: %d", omp_get_thread_num()); //print thread id to check if oprnmp works
+    }
+
+    return 0;
+}
