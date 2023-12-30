05edaabcb66cb596403cd5d04e04eb80a149f8bf
Riccardo Nicolin <riccardo.nicolin@studenti.unitn.it>
2023-12-30 11:21:11 +0100

    debug 10

--- FFT_parallel/pragma.c
+++ FFT_parallel/pragma.c
@@ -14,12 +14,21 @@
 #include <mpi.h>
 
 int main(){
+    int comm_sz;
+	int my_rank;
+
+	MPI_Init(NULL, NULL);
+	MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     int i;
+    if (my_rank == 0)
     {
         #pragma omp for
         for (i=0; i<5; i++){
             printf("thread id: %d \n", omp_get_thread_num()); //print thread id to check if oprnmp works
         }
+    }else{
+        printf("I'm not rank 0 \n")
     }
     return 0;
 }
